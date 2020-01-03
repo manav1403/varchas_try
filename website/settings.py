e281661bf6815e11e5b9593de8f0d38550b9ed5d
manav1403 <manavavni@gmail.com>
2020-01-03 11:46:18 +0530

    database_w

--- website/settings.py
+++ website/settings.py
@@ -76,16 +76,14 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-import dj_database_url
-db_from_env =dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+import psycopg2
 
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
