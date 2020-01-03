61adc414db105687092cca4263c6a6d7da903c1e
manav1403 <manavavni@gmail.com>
2020-01-04 04:46:34 +0530

    added cricket and end game

--- login/views.py
+++ login/views.py
@@ -12,43 +12,68 @@ def game(request):
             obj=match_list.objects.filter(game_id=request.POST['id'])[0]
             obj.live_status=True
             obj.save()
-            g=game_list(game_id=request.POST['id'])
+            g=game_list(game_id=request.POST['id'],team1_score="0/0",team2_score="0/0")
             g.save()
         if( request.POST['type']=="clock"):
             obj=match_list.objects.filter(game_id=request.POST['id'])[0]
             obj.ac_start_time=request.POST['data']
             obj.save()
+        if( request.POST['type']=="end"):
+            obj=match_list.objects.filter(game_id=request.POST['id'])[0]
+            obj.live_status=False
+            obj.end_status=True
+            obj.final_discription=request.POST['comments']
+            obj.winner=request.POST['winner']
+            g=game_list.objects.filter(game_id=request.POST['id']).order_by('-id')[0]
+            obj.final_team1=g.team1_score
+            obj.final_team2=g.team2_score
+            obj.save()
+            return redirect("/logedin")   
     else:
-        request.redirect("/logedin")        
+        return redirect("/login/vol/logedin")        
     return HttpResponse("null")
 
-def update(request):
+def update(request):#for cricket and basketball(10 and 11)
     p=request.POST
     
     if(User.is_authenticated and request.method=='POST'):
         print(p['request'])
         if(p['request'] == '1'):
-
             ch=game_list.objects.filter(game_id=p['id']).order_by('-id')[0]
-        
+            a=0
+            b=0    
             if(p['team']=='1'):
-             ch.team1_score=int(p['value'])+int(ch.team1_score)
+             print(p['value'])
+             team1_score=ch.team1_score.split('/')
+             ch.team1_score=str(int(p['value'])+int(team1_score[0]))
+             if(int(p['id'])//10000==10):
+                wkt=str(int(team1_score[1])+int(p['wicket']))
+                ch.team1_score=ch.team1_score+"/"+wkt
+                ch.overs_1=p['overs']
              a=ch.team1_score
+             b=ch.overs_1
             if(p['team']=='2'):
-             ch.team2_score=int(p['value'])+int(ch.team2_score)
+             team2_score=ch.team1_score.split('/')
+             ch.team2_score=str(int(p['value'])+int(team2_score[0]))
+             if(int(p['id'])//10000==10):
+                ch.team2_score=ch.team2_score+"/"+p['wicket']
+                ch.overs_2=p['overs']
              a=ch.team2_score
-            obj=game_list(game_id=ch.game_id,team1_score=ch.team1_score,team2_score=ch.team2_score)
+             b=ch.overs_2
+            obj=game_list(game_id=ch.game_id,team1_score=ch.team1_score,team2_score=ch.team2_score,overs_1=ch.overs_1,overs_2=ch.overs_2)
             obj.save()
-            return JsonResponse({'status':"success",'score':a})
+            return JsonResponse({'status':"success",'score':a,'overs':b})
         if(p['request'] == '2'):
             ch=game_list.objects.filter(game_id=p['id']).order_by('-id')[0]
             ch.delete()
             ch=game_list.objects.filter(game_id=p['id']).order_by('-id')[0]
             if(p['team']=='1'):
              a=ch.team1_score
+             b=ch.overs_1
             if(p['team']=='2'):
              a=ch.team2_score
-            return JsonResponse({'status':"success",'score':a})
+             b=ch.overs_2
+            return JsonResponse({'status':"success",'score':a,'overs':b})
 
 
 def vol_logedin(request):
@@ -61,12 +86,10 @@ def vol_logedin(request):
     if(User.is_authenticated and request.method=='POST'):
         
         c=int(request.POST['id'])//10000
-        ch=game_list.objects.filter(game_id=request.POST['id']).order_by('-id')
-        dat=0
-        if(ch):
-            dat=ch[0]
+        ch=game_list.objects.filter(game_id=request.POST['id']).order_by('-id')[0]
         game=match_list.objects.filter(game_id=request.POST['id'])[0]
-        return render(request,"games.html",{'c':11,'game':game,'dat':dat})
+        
+        return render(request,"games.html",{'c': c,'game':game,'dat':ch})
 
 def live(request):
     daw = match_list.objects.filter(live_status=True,end_status=False)
